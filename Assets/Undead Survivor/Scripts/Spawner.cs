fea6d3cfdf1834ae8dddd7044eb6ed8685c5ace6
j-jiwon <devziwon@gmail.com>
2023-06-07 05:33:49 +0900

    add level system

--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
+    int level;
     float timer;
 
     void Awake()
@@ -14,17 +16,28 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
 
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > 0.2f)
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
+        if (timer > spawnData[level].spawnTime)
         {
             Spawn();
             timer = 0;
         }      
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public int spriteType;
+    public float spawnTime;
+    public int health;
+    public float speed;
 }
