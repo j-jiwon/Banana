e93722343c554037361881e8ac510df17693781a
j-jiwon <devziwon@gmail.com>
2023-06-07 04:27:48 +0900

    add monsters

--- Assets/Undead Survivor/Scripts/Reposition.cs
+++ Assets/Undead Survivor/Scripts/Reposition.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -13,7 +19,7 @@ public class Reposition : MonoBehaviour
         Vector3 myPos = transform.position;
         float diffX = Mathf.Abs(playerPos.x - myPos.x);
         float diffY = Mathf.Abs(playerPos.y - myPos.y);
-        
+
         Vector3 playerDir = GameManager.instance.player.inputVec;
         float dirX = playerDir.x < 0 ? -1 : 1;
         float dirY = playerDir.y < 0 ? -1 : 1;
@@ -22,15 +28,15 @@ public class Reposition : MonoBehaviour
         {
             case "Ground":
                 if (diffX > diffY)
-                {
                     transform.Translate(Vector3.right * dirX * 40);
-                }
                 else
-                {
                     transform.Translate(Vector3.up * dirY * 40);
-                }
                 break;
             case "Enemy":
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+                }
                 break;
         }
     }
